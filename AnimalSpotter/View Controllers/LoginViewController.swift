8855d223f2c56042f3db5d90f535cb9abb4b9e4f
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-11 14:05:37 -0400

    done with day 1

--- AnimalSpotter/View Controllers/LoginViewController.swift
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -35,9 +35,53 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         // perform login or sign up operation based on loginType
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
+            let user = User(username: username, password: password)
+            
+            if loginType == .signUp {
+                apiController?.signUp(with: user, completion: { error in
+                    if let error = error {
+                        NSLog("Error occured during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            let alertController = UIAlertController(title: "Sign Up Successful", message: "Now Please log in", preferredStyle: .alert)
+                            let okbutton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alertController.addAction(okbutton)
+                            self.present(alertController, animated: true) {
+                                self.loginType = .signIn
+                                self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                                self.signInButton.setTitle("Sign In", for: .normal)
+                            }
+                        }
+                    }
+                })
+            } else {
+                // user wants to sign in
+                apiController?.signIn(with: user, completion: { error in
+                    if let error = error {
+                        NSLog("Error occuring during sign in: \(error)")
+                        
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                })
+            }
+        }
     }
     
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
         // switch UI between login types
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
     }
 }
